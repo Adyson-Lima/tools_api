1a9e0c3fd00d8e2dad1d0a5428979fd630405374
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-10 06:54:48 -0300

    12° commit - tdd controller: Consegue retornar uma tool especifica e retornar status 200? RED

--- spec/controllers/v1/tools_controller_v1_spec.rb
+++ spec/controllers/v1/tools_controller_v1_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe Api::V1::ToolsController, type: :controller do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "GET /api/v1/tools/id" do
+    it "Consegue retornar uma tool especifica e retornar status 200?" do
+      get :show, params: {id: @tool.id}
+      expect(response.body).to include_json(id: @tool.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
