6a83c78b4ae5c5d382c11f1367be2acdee31cd04
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-10 07:28:22 -0300

    20° commit - seeds.rb configurado

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Gerando tools...'
+
+5.times do |i|
+  Tool.create(
+    name: ["multimetro", "osciloscopio"].sample ,
+    description: ["ferramenta de manutenção","ferramenta de teste"].sample
+    )
+end
+
+puts 'tools gerados com sucesso!'
