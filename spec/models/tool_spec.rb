8aa1d863997e39440a0b49aaf68aa0f3e60b3d70
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-10 06:17:24 -0300

    2° commit - tdd model: name consegue ser preenchido? RED

---
+++ spec/models/tool_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe Tool, type: :model do
+
+  before{@tool = Tool.new}
+
+  describe "Teste de preenchimento dos campos do model Tool" do
+
+    it "name consegue ser preenchido?" do
+      @tool.name = ""
+      expect(@tool.name).to eq("multimetro")
+    end
+
+  end
+
+end
